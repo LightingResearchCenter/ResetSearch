16ae36fae3ceba14c4e03859d8d17c31ffbd787d
Geoffrey Jones <jonesg5@rpi.edu>
2015-08-25 15:18:53 -0400

    Created searchFiles

---
+++ searchFiles.m
@@ -0,0 +1,40 @@
+function resetInventory = searchFiles(dataPathArray,logPathArray)
+%SEARCHFILES Summary of this function goes here
+%   Detailed explanation goes here
+
+% Count the number of data set paths
+nSet = numel(dataPathArray);
+
+% Initialize variables
+nResets         = zeros(nSet,1);
+nUnwritten      = zeros(nSet,1);
+nReadings       = zeros(nSet,1);
+status          = cell(nSet,1);
+sn              = zeros(nSet,1);
+start_datenum	= zeros(nSet,1);
+interval_sec	= zeros(nSet,1);
+downloaded      = false(nSet,1);
+battery_mV      = zeros(nSet,1);
+
+% Open the pool
+gcp;
+
+% Dive on in
+parfor iSet = 1:nSet
+    thisData = dataPathArray{iSet};
+    thisLog  = logPathArray{iSet};
+    
+    [~,nResets(iSet),nUnwritten(iSet),nReadings(iSet)] = readData(thisData);
+    [status{iSet},sn(iSet),start_datenum(iSet),interval_sec(iSet),...
+        downloaded(iSet),battery_mV(iSet)] = readLog(thisLog);
+end
+
+% Calculate duration of use in days
+duration_days = interval_sec.*nReadings/(60*60*24);
+
+% Bundle reset counts and meta data as a table
+resetInventory = table(nResets,nUnwritten,nReadings,status,sn,...
+    start_datenum,interval_sec,downloaded,battery_mV,duration_days);
+
+end
+
