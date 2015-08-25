5f989af87e24cbe859882bf8c274ba052a3e620d
Geoffrey Jones <jonesg5@rpi.edu>
2015-08-25 13:45:43 -0400

    Added readData to test script

--- test.m
+++ test.m
@@ -2,6 +2,9 @@ close all
 clear
 clc
 
-logPath = 'test-Log.txt';
+logPath = 'test-LOG.txt';
+dataPath = 'test-DATA.txt';
 
 [status,id,startTime,logInt,downloaded,battery_mV] = readLog(logPath);
+
+[uncalibrated,nResets,nUnwritten,nReadings] = readData(dataPath);
